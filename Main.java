2f90bdff31d0bc5202ff3eadaf4b742222487e45
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-17 23:10:13 +0900

    java基礎

---
+++ Main.java
@@ -0,0 +1,25 @@
+// Java基礎学習
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello paiza");
+        System.out.println("こんにちは、パイザ");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello paiza");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Good morning");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("世界の皆さん、こんにちは");
+    }
+}
--- java_kiso.java
+++
@@ -1,7 +0,0 @@
-// Java基礎学習
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello paiza");
-        System.out.println("こんにちは、パイザ");
-    }
-}
