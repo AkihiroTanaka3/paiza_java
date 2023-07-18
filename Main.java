983da3c7120287595901e24414ce5ef87ee7378a
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-18 22:47:42 +0900

    java基礎

--- Main.java
+++ Main.java
@@ -23,3 +23,42 @@ public class Main {
         System.out.println("世界の皆さん、こんにちは");
     }
 }
+
+// 間違いやすいポイント
+// 小文字と大文字、セミコロン；とコロン：
+// 半角と全角、括弧とダブルクオーテーションなど
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+}
+
+// 間違い探し
+public class Main {
+    public static void main(String[] args) {
+        // System.out.pirntln("Hello paiza"); スペルミス
+        System.out.println("Hello paiza");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Hello paiza") セミコロンが無い
+        System.out.println("Hello paiza");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Hello paiza); ダブルクオーテーションがない
+        System.out.println("Hello paiza");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println（"Hello paiza"); 大文字の（になっている
+        System.out.println("Hello paiza");
+    }
+}
