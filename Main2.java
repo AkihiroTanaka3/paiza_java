70319ffb0b5bdaeba390a9524bf8d066fe964e6c
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-20 23:39:34 +0900

    数値と文字列

--- Main2.java
+++ Main2.java
@@ -3,5 +3,26 @@ public class Main2 {
         System.out.println("Hello world");
         System.out.println(100 + 30);
         System.out.println("100 + 30");
+        System.out.println(12345);
+        System.out.println(12345 + 6789);
+        System.out.println("12345 + 6789");
     }
 }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println(12345);
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println(12345 + 6789);
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("12345 + 6789");
+//     }
+// }
