489e99d34ea009c862abb9b5357c0f0ca1aa7bdc
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-20 22:59:00 +0900

    テスト

---
+++ Main1.java
@@ -0,0 +1,7 @@
+// ファイル名: Main1.java
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("Hello paiza");
+        System.out.println("こんにちは、パイザ");
+    }
+}
