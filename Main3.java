8f70feea2e3a88a22944bda9ee8658d51f9619dd
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-21 15:48:59 +0900

    計算

---
+++ Main3.java
@@ -0,0 +1,7 @@
+public class Main3 {
+    public static void main(String[] args) {
+    System.out.println(12345 - 6789);
+    System.out.println(12345 * 6789);
+    System.out.println(12345 / 5);
+    }
+}
