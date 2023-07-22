eed8c30090be6ae735e90123d87d847cd4c059e4
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-23 00:05:50 +0900

    文字列

---
+++ Main5.java
@@ -0,0 +1,10 @@
+// 文字列を受け取る
+import java.util.*;
+
+public class Main5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String text = scan.next();
+        System.out.println("Hello " + text);
+    }
+}
