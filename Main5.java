8feaadd96b7f0003e09ca27a390790f771f7c08c
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-23 00:11:04 +0900

    文字列

--- Main5.java
+++ Main5.java
@@ -5,6 +5,10 @@ public class Main5 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String text = scan.next();
+        String text2 = scan.next();
+        System.out.println(text);
+        System.out.println(text2);
         System.out.println("Hello " + text);
+        System.out.println("Hello " + text2);
     }
 }
