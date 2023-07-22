01c3c36bd1b6c29b19a188ca4e3dfa4b0a442c9c
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-22 23:54:39 +0900

    変数

---
+++ Main4.java
@@ -0,0 +1,11 @@
+// 変数にデータを入れる
+public class Main4 {
+    public static void main(String[] args) {
+        String greeting = "Hello paiza";
+        System.out.println(greeting);
+        System.out.println(greeting);
+        
+        Integer number = 100;
+        System.out.println(number);
+    }
+}
