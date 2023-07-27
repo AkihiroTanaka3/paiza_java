c690c6f53f886c28c780f3b2199532f4dd405930
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-27 21:14:48 +0900

    文字列の一致を確認

---
+++ Main9.java
@@ -0,0 +1,20 @@
+// 文字列の一致を判定する
+import java.util.*;
+
+public class Main9 {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            
+        String name = scan.next();
+        System.out.println("Hello " + name);
+        
+            if (name.equals("Java")) {
+                System.out.println("Welcom");
+            } else if (name.equals("JAVA")) {
+                System.out.println("Good morning");
+            } else {
+                System.out.println("Goodbye");
+            }
+        }
+    }
+}
