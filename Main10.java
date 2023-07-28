e682eb20731e74ba8fe73cc9cc8c43b90a23eb24
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-29 01:07:17 +0900

    同じ処理を何度も繰り返す

---
+++ Main10.java
@@ -0,0 +1,10 @@
+// 同じ処理を何度も繰り返す
+public class Main10 {
+    public static void main(String[] args) {
+        String greeting = "Hello paiza";
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.println(greeting + ":" + i);
+        }
+    }
+}
