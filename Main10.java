fe5874340a1a268ff44e4a0c4e07816d66a91f25
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-29 01:10:00 +0900

    100回

--- Main10.java
+++ Main10.java
@@ -3,7 +3,7 @@ public class Main10 {
     public static void main(String[] args) {
         String greeting = "Hello paiza";
         
-        for (int i = 0; i < 3; i++) {
+        for (int i = 1; i < 101; i++) {
             System.out.println(greeting + ":" + i);
         }
     }
