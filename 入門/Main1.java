593b17092617f9fd41991bb4a33323644656a312
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-01 21:53:59 +0900

    HTMLで表示

---
+++ 入門/Main1.java
@@ -0,0 +1,10 @@
+package 入門;
+
+// HTMLを表示する
+public class Main1 {
+	public static void main(String[] args) {
+		System.out.print("<h1>hello world</h1>");
+		System.out.print("<p>世界の皆さん、");
+		System.out.print("<b>こんにちは</b></p>");
+	}
+}
