75ee6e17cb1cca89627c665c9c30e9f74eb0bd6a
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-01 22:00:20 +0900

    変数を使う

---
+++ 入門/Main2.java
@@ -0,0 +1,11 @@
+package 入門;
+// 変数を使う
+public class Main2 {
+	public static void main(String[] args) {
+	    String player = "勇者";
+	   // System.out.println(player);
+		System.out.println(player + "は、荒野を歩いていた");
+		System.out.println(player + "は、モンスターと戦った");
+		System.out.println(player + "は、モンスターをたおした");
+	}
+}
