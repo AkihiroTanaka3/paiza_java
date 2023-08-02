d5449b689a8755708b233eb6d6236ec90e83356a
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-03 00:12:34 +0900

    サイコロ

--- 入門/Main3.java
+++ 入門/Main3.java
@@ -8,6 +8,10 @@ public class Main3 {
 		int number = (int)rand; //型変換
 // 		System.out.println(number);
 		System.out.println("スライムが" + number + "匹あらわれた");
+        double rand1 = Math.random() * 6 + 1;
+		int number1 = (int)rand1;
+		System.out.println("サイコロの目は" + number1 +"です");
 	}
+    
 }
 
