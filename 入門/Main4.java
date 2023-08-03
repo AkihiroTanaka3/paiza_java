e08d97771e05fb6ee63cebb26bfdb4d41d2d4ee8
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-03 17:34:42 +0900

    計算

---
+++ 入門/Main4.java
@@ -0,0 +1,10 @@
+package 入門;
+
+public class Main4 {
+    //計算する
+	public static void main(String[] args) {
+		// ここに計算式を書いて、出力する
+		int number = 1234 * 5678 / 2;
+		System.out.println(number);
+	}
+}
