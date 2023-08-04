a4f5664fec31ccace3311e0c9e994d55cd604f80
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-05 04:22:52 +0900

    if文による条件分岐

---
+++ 入門/Main7.java
@@ -0,0 +1,15 @@
+package 入門;
+
+// if文による条件分岐
+public class Main7 {
+	public static void main(String[] args) {
+	    // int number = 1;
+        int number = (int)(Math.random() * 2 + 1);
+        if (number == 1) {
+            System.out.println(number + "スキ！"); // 条件式が成立した時の処理
+        } else {
+            System.out.println(number + "キライ！"); //条件が成立しなかった時の処理
+        }
+	}
+}
+
