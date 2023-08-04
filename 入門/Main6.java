2aeb248c6ffbc4f97851975b5f3e8a099b107984
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-04 16:07:52 +0900

    データの種類

---
+++ 入門/Main6.java
@@ -0,0 +1,14 @@
+package 入門;
+
+// データの種類
+public class Main6 {
+	public static void main(String[] args) {
+		int number = 100 + 30;	// 数値
+		String text = "ハロー" + "paiza";	// 文字列
+		System.out.println(number);
+		System.out.println(text);
+		System.out.println(number + text);
+		System.out.println(number + 20); //データの型は数値のまま
+	}
+}
+
