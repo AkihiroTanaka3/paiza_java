ad325447edd08463e96815a0915f0e945f903715
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-06 20:40:03 +0900

    比較演算子

---
+++ 入門/Main9.java
@@ -0,0 +1,16 @@
+package 入門;
+
+// if文による条件分岐　比較演算子
+public class Main9 {
+	public static void main(String[] args) {
+		int time = (int)(Math.random() * 24 + 1);
+		    System.out.print(time + "時です。");
+		if (time < 12) {
+		    System.out.println("午前中");
+		} else if (time == 12) {
+		    System.out.println("正午");
+		} else if (time > 12) {
+		    System.out.println("午後");
+		}
+	}
+}
