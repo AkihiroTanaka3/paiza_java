0c24ae2de27c94f2acd3431e3a616b60e04da2a0
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-08 19:03:07 +0900

    比較演算子

--- 入門/Main11.java
+++ 入門/Main11.java
@@ -11,11 +11,19 @@ package 入門;
 public class Main11 {
 	public static void main(String[] args) {
         int hit = (int)(Math.random() * 10 + 1);
-        System.out.println(hit);
+        // System.out.println(hit);
         if (hit < 6) {
             System.out.println("スライムに、" + hit + "のダメージを与えた！");
         } else {
             System.out.println("クリティカルヒット！スライムに、100のダメージを与えた！");
         }
+
+        int add = (int)(Math.random() * 2 + 1); // この行を修正して1から2のサイコロになるようにする
+		System.out.println("追加のサイコロは、" + add);
+		if (add == 1) { // この行の条件式を修正
+			System.out.println("追加攻撃!スライムに、10のダメージを与えた!!!");
+		} else {
+			System.out.println("追加攻撃に失敗!");
+		}
 	}
 }
