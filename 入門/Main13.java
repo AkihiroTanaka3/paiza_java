2f6b63c7c6dfeca739c7287d757e20eaa047a3d3
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-10 20:25:50 +0900

    複数の条件を組み合わせる

---
+++ 入門/Main13.java
@@ -0,0 +1,25 @@
+package 入門;
+
+// 複数の条件を組み合わせる - AND
+
+public class Main13 {
+    public static void main(String[] args) {
+        int number1 = (int)(Math.random() * 10 + 1);
+        // int number2 = 1;
+        if (number1 <= 8 && number1 >= 3) {
+            System.out.println(number1 + "スキ！"); //条件が成立した時
+        } else {
+            System.out.println(number1 + "キライ"); //条件が成立しなかった時
+        }
+        
+        double rand = Math.random() * 100 + 1;
+        int number3 = (int)rand;
+        System.out.println(number3);
+        if (number3 >= 30 && number3 <= 60) {
+            System.out.println("あたり");
+        } else {
+            System.out.println("残念");
+        }
+    }
+}
+
