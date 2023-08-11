4ff8614a69e98aaa7ad41ee67fe4483576831575
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-11 16:26:15 +0900

    複数の条件を組み合わせる　or

---
+++ 入門/Main14.java
@@ -0,0 +1,24 @@
+package 入門;
+
+// 複数の条件を組み合わせる - OR
+public class Main14 {
+    public static void main(String[] args) {
+        int number1 = 1;
+        int number2 = 0;
+        if (number1 == 1 || number2 == 1) {
+            System.out.println("スキ！");
+        } else {
+            System.out.println("キライ");
+        }
+        
+        double rand = Math.random() * 100 + 1;
+        int number3 = (int)rand;
+        System.out.println(number3);
+        if (number3 <= 30 || number3 >= 60) {
+            System.out.println("あたり");
+        } else {
+            System.out.println("残念！");
+        }
+    }
+}
+
