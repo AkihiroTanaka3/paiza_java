b68431c549da91318e45f24cdd5e431bd6ec7684
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-13 07:20:01 +0900

    条件の評価結果、論理値

---
+++ 入門/Main15.java
@@ -0,0 +1,17 @@
+package 入門;
+
+// 条件の評価結果
+
+public class Main15 {
+    public static void main(String[] args) {
+        int number = 1;
+        boolean flag = number ==1;
+        if (flag) {
+            System.out.println("スキ！");
+        } else {
+            System.out.println("キライ");
+        }
+        System.out.println(flag);
+    }
+}
+
