caad34e19e7591a7adb46d357dfcecbfad98d411
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-17 14:58:46 +0900

    標準入力

---
+++ 入門/Main19.java
@@ -0,0 +1,13 @@
+package 入門;
+
+// 標準入力
+import java.util.*;
+
+public class Main19 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+        String data = sc.next();
+        System.out.println("hello " + data);
+        }
+    }
+}
