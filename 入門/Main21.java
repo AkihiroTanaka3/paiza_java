ccf1de2aa051dd2d720333de8575e941c0a86474
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-20 19:08:35 +0900

    配列

--- 入門/Main21.java
+++ 入門/Main21.java
@@ -14,5 +14,12 @@ public class Main21 {
         System.out.println(team[0]);
         System.out.println(team[1]);
         System.out.println(team[2]);
+
+        String[] array = {"戦士","侍","僧侶","魔法使い"};
+        // この下で、arrayを出力してみよう
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
     }
 }
