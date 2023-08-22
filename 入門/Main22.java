6fb7d952f915e2f0d987deeab52128677b7b0b87
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-22 16:33:16 +0900

    二次元配列

--- 入門/Main22.java
+++ 入門/Main22.java
@@ -29,5 +29,19 @@ public class Main22 {
         System.out.print(teams[0][0]);
         System.out.println("");
         System.out.println("---");
+
+        String[] item1 = {"木の棒", "こん棒"};
+        String[] item2 = {"おにぎり", "おにぎり"};
+        String[] item3 = {"毒消し", "薬草"};
+
+        // item1 ~ 3を、basket配列に代入してください。
+
+        String[][] basket = {item1, item2, item3};
+        System.out.println(basket[0][0]);
+        System.out.println(basket[0][1]);
+        System.out.println(basket[1][0]);
+        System.out.println(basket[1][1]);
+        System.out.println(basket[2][0]);
+        System.out.println(basket[2][1]);
     }
 }
