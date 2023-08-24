8088fe37c8d1a53669f5e0107e455ef2b3927b03
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-24 23:19:30 +0900

    メソッド

--- 入門/Main23.java
+++ 入門/Main23.java
@@ -6,10 +6,13 @@ public class Main23 {
     public static void main(String[] args) {
         System.out.println("hello world");
         sayHello();
-        sayHello();
+        sayHello("java");
     }
     
     public static void sayHello() {
         System.out.println("hello paiza");
+    }
+    public static void sayHello(String name) {
+        System.out.println("hello " + name);
     }
 }
