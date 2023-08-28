dc2bede10389d6e90c4bf7fea09cc7dc64cbcdab
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-08-28 20:45:56 +0900

    クラス作成

--- 入門/Main24.java
+++ 入門/Main24.java
@@ -6,12 +6,26 @@ public class Main24 {
     public static void main(String[] args) {
         Player player1 = new Player();
         player1.walk();
+        // この下でGreetingオブジェクトをつくり、メソッドを呼び出す
+        Greeting greeting1 = new Greeting();
+        greeting1.sayHello();
+        Greeting greeting2 = new Greeting();
+        greeting2.sayHello("java");
     }
 }
     
 class Player { 
     public void walk() {
         System.out.println("勇者は荒野を歩いていた。");
+    }
+}
+
+class Greeting {
+    public void sayHello() {
+        System.out.println("hello paiza");
+    }
+    public void sayHello(String name) {
+        System.out.println("hello " + name);
     }
 }
 
